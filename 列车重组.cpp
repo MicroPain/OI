cbaf826984c5f04e057c64073b0d006b1ea228e1
MicroPain <44607638+MicroPain@users.noreply.github.com>
2018-11-27 21:08:27 +0800

    Add files via upload

---
+++ 列车重组.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int n,sum=0;
+	cin>>n;
+	const int N=n;
+	int Ar[N];
+	for(int i=0;i<N;i++)
+	{
+		cin>>Ar[i];
+	} 
+	for(int j=N;j>0;j--)
+		for(int i=0;i<j-1;i++)
+		{
+			if(Ar[i]>Ar[i+1]){
+				swap(Ar[i],Ar[i+1]);
+				sum++;
+			}
+		}
+	cout<<sum;
+	return 0;
+ } 

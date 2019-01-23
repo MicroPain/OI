28c22c87690290b741852fc3cb7856423be47b7d
MicroPain <44607638+MicroPain@users.noreply.github.com>
2019-01-23 21:15:45 +0800

    Update A+B高精度.cpp

--- A+B高精度.cpp
+++ A+B高精度.cpp
@@ -1,51 +1,52 @@
 #include<bits/stdc++.h>
-#include<cstring>
-#define MAX 520
-using  namespace std;
-int MY_scan(int Ar[])
-{
-    string s;
-    cin>>s;
-    int K=s.length();		
-    for(int i=0;i<K;i++)
-    {
-        Ar[i]=s[K-i-1]-'0';
-    }
-    return K;
-}
-int MY_plus(int a[],int b[],int r[],int len)
-{
-    int x=0,sign=0;
-    for(int i=0;i<=len;i++)
-    {
-        r[i]+=a[i]+b[i]+x;
-        x=r[i]/10;
-        r[i]%=10;
-    }
-    for(int i=MAX-1;i>=0;i--)
-    {
-        if(r[i]!=0)
-        {
-            sign=i;	
-            break;
-        }
-    }
-    for(int i=sign;i>=0;i--)
-        cout<<r[i];
-    return 0;
-}
+using namespace std;
+int SCAN(int x[]);
+int ADD(int c[],int a[],int b[]);
 int main()
 {
-    int Ar[MAX],Ar1[MAX],res[MAX];
-    int len1=0,len2=0;
-    memset(Ar,0,sizeof(Ar));
-    memset(Ar1,0,sizeof(Ar1));
-    memset(res,0,sizeof(res));
-    len1=MY_scan(Ar);
-    len2=MY_scan(Ar1); 
-    if(len1>=len2)
-        MY_plus(Ar,Ar1,res,len1);
- 	else 
-        MY_plus(Ar1,Ar,res,len2);
-    return 0;
+	int a[100];
+	int b[100];
+	int c[100];
+	memset(a,0,sizeof(a));
+	memset(b,0,sizeof(b));
+	memset(c,0,sizeof(c));
+	SCAN(a);
+	SCAN(b);
+	ADD(c,a,b);
+	return 0;
+}
+int SCAN(int x[])
+{
+	string s;
+	cin>>s;
+	x[0]=s.length();
+	for(int i=1;i<=x[0];i++)
+	{
+		x[i]=s[x[0]-i]-'0';
+	}
+	return 0;
+}
+int ADD(int c[],int a[],int b[])
+{
+	int k=max(a[0],b[0]);
+	for(int i=1;i<=k+1;i++)
+	{
+		c[i]+=a[i]+b[i];
+		if(c[i]>=10)
+		{
+			c[i+1]++;
+			c[i]%=10;	
+		}
+	}
+	for(int i=99;i>=1;i--)
+	{
+		if(c[i]!=0)
+		{
+			c[0]=i;
+			break;
+		}
+	}
+	for(int i=c[0];i>=1;i--)
+		printf("%d",c[i]);
+	return 0;
 }

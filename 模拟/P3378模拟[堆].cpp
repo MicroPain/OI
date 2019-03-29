612512d333316def91bc2ff1bf3fd8a8a43bc212
MicroPain <44607638+MicroPain@users.noreply.github.com>
2019-03-29 18:29:29 +0800

    Revert "Update P3378模拟[堆].cpp"
    This reverts commit 45d3b5a816db1ffcd840f3578577a9f2af78a8a4.

--- 模拟/P3378模拟[堆].cpp
+++ 模拟/P3378模拟[堆].cpp
@@ -6,56 +6,56 @@ int mOVEaHEAD(int A[],int x);
 int up=0,down=1;
 int main()
 {
-    int N;
-    int A[1000001];
-    cin>>N;
-    int CMD[N][2];
-    for(int i=0;i<N;i++)
-    {
-        cin>>CMD[i][0];
-        if(CMD[i][0]==1)
-        {
-            cin>>CMD[i][1];
-        }
-    }
-    for(int i=0;i<N;i++)
-    {
-        
-        if(CMD[i][0]==1)
-        {
-            up++;
-            A[up]=CMD[i][1];
-        }
-        if(CMD[i][0]==3)
-        {
-            int x_loc=mIN(A);
-            mOVEaHEAD(A,x_loc);
-        }
-        if(CMD[i][0]==2)
-        {
-            int x_loc=mIN(A);
-            printf("%d\n",A[x_loc]);
-        }
-    }
-    return 0;
+	int N;
+	int A[10001];
+	scanf("%d",N);
+	int CMD[N][2];
+	for(int i=0;i<N;i++)
+	{
+		printf("%d",CMD[i][0]);
+		if(CMD[i][0]==1)
+		{
+			cin>>CMD[i][1];
+		}
+	}
+	for(int i=0;i<N;i++)
+	{
+		
+		if(CMD[i][0]==1)
+		{
+			up++;
+			A[up]=CMD[i][1];
+		}
+		if(CMD[i][0]==3)
+		{
+			int x_loc=mIN(A);
+			mOVEaHEAD(A,x_loc);
+		}
+		if(CMD[i][0]==2)
+		{
+			int x_loc=mIN(A);
+			printf("%d\n",A[x_loc]);
+		}
+	}
+	return 0;
 }
 int mIN(int A[])
 {
-    int x=down;
-    for(int i=down;i<=up;i++)
-    {
-        if(A[i]<A[x])
-            x=i;	
-    }
-    return x;
+	int x=down;
+	for(int i=down;i<=up;i++)
+	{
+		if(A[i]<A[x])
+			x=i;	
+	}
+	return x;
 }
 int mOVEaHEAD(int A[],int x)
 {
-    for(int i=x+1;i<=up;i++)
-    {
-        A[i-1]=A[i];
-        A[i]=0;
-    }
-    up--;
-    return 0;
+	for(int i=x+1;i<=up;i++)
+	{
+		A[i-1]=A[i];
+		A[i]=0;
+	}
+	up--;
+	return 0;
 }

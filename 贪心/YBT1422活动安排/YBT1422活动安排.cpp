5f38c640c81eee3573a83917689c3b3b54676833
MicroPain <44607638+MicroPain@users.noreply.github.com>
2019-05-03 21:22:34 +0800

    Update YBT1422活动安排.cpp

--- 贪心/YBT1422活动安排/YBT1422活动安排.cpp
+++ 贪心/YBT1422活动安排/YBT1422活动安排.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int mm_sort(int Ar[][3],int a,int b);
 int mm_ischosen(int Ar[][3],int a,int b); 
 int main(){
-	int n,sum=0;		//共有n个活动,入选的活动有sum个 
+	int n,sum=0;	//����n���,��ѡ�Ļ��sum�� 
 	cin>>n;				
-	int Ar[n+1][3];		//存放每个活动的始末时间 
+	int Ar[n+1][3];	//���ÿ�����ʼĩʱ��
 	for(int i=1;i<=n;i++){
 		for(int j=0;j<=1;j++){
 			cin>>Ar[i][j];
 		}
-		Ar[i][2]=1;		//用于标记该区间是否被排除或选入 (1为待定)
+		Ar[i][2]=1;	//���ڱ�Ǹ������Ƿ��ų���ѡ�� (1Ϊ����)
 	}
-	mm_sort(Ar,1,n);	//按初始位置由小到大给Ar排序 
+	mm_sort(Ar,1,n);//����ʼλ����С�����Ar���� 
 	for(int i=1;i<=n;i++){
 		if(Ar[i][2]!=0){
 			Ar[i][2]=2;
